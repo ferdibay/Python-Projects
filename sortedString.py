89e8ace20f97bcf2ff4f385cd9efa3cffe147506
ferdi <ferdigit@gmail.com>
2022-03-12 02:04:49 +0300

    Update sortedString.py

--- sortedString.py
+++ sortedString.py
@@ -1,30 +1,3 @@
-
-
-
-
-"""
-def fibonacci(n):
-    #eleman=int(input("eleman sayısı:")) #bu durumda n yerine eleman yazılırdı..
-    a=1
-    b=1
-
-    if (n>=2):
-        print(a)
-        print(b)
-        for i in range(n-2):
-            c=a+b
-            a=b
-            b=c
-            print(c)
-    else:
-        print("fibonacci en az 2 elmandan oluşur!")
-
-fibonacci(10)                
-"""
-
-
-
-
 #Bir Stringteki harfler alfabetik sıradaysa True döndüren fonksiyonu yazınız..
 def siraliMi():
     kelime=input("Bir kelime yazın:")
